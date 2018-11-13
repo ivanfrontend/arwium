5a59851e16a8a1d1688dcfac802af3c64a63c4f9
ivanfrontend <ivanfrond111950@mail.ru>
2018-11-13 13:04:42 +0300

    111

--- js/main.js
+++ js/main.js
@@ -383,6 +383,13 @@ jQuery(document).ready(function() {
 
 
 
+	jQuery(".only_number").keypress(function(e) {
+if (e.which != 8 && e.which != 0 && e.which != 46 && (e.which < 48 || e.which > 57)) {
+return false;
+}
+});
+
+
 
 
 
